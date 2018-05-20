3c1c193a23ed10a7b4f4373eedf64201be5f0fdc
masoud aghamohamadian <masoudas@hotmail.com>
2018-05-20 14:06:14 +0430

    Indexers and properties were separated into two projects.

--- IndexersAndProperties/IndexersAndProperties/Indexers.cs
+++ IndexersAndProperties/IndexersAndProperties/Indexers.cs
@@ -6,6 +6,131 @@ namespace IndexersAndProperties
 {
     class Indexers
     {
+        /* Indexers provide the mechanism by which an object can be indexed like an array */
+        /* Here's the point: Suppose you want to create an array of a particular object, like Obj. So long as you have not defined indexers,
+         * you cannot say Obj[] obj = new Obj[5]. The part on the right would result in an error. This is the reason why we need indexers.
+           Clearly, we cannot have more than one indexer for a class*/
 
+        double[] array;    // This array is defined private!
+
+        public Indexers(int size)
+        {
+            array = new double[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Math.Cosh(i);
+            } 
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < array.Length)
+                    return (array[index]);
+                else
+                    return (-1);
+            }
+            set
+            {
+                if (index >= 0 && index < array.Length)
+                    array[index] = value;   // This value is a predefined thing!
+            }
+        }
+
+        // Indexers can be overloaded, of course not in such a stupid manner, but anyhow they can be!
+        public double this[int index1, int index2]   
+        {
+            get
+            {
+                if (index1 >= 0 && index1 < array.Length)
+                    return (array[index2]);
+                else
+                    return (-1);
+            }
+            set
+            {
+                if (index1 >= 0 && index1 < array.Length)
+                    array[index2] = value;   // This value is a predefined thing!
+            }
+        }
+
+        public void printStatement()
+        {
+            Console.WriteLine("La La La something and noodles in strings");
+        }
+
+    }
+
+    class IndexerWithNoArray
+    {
+        public int this[int index]
+        {
+            // Compute and return power of 2.
+            get
+            {
+                if ((index >= 0) && (index < 16)) return pwr(index);
+                else return -1;
+            }
+            // There is no set accessor.
+        }
+
+        int pwr(int p)
+        {
+            int result = 1;
+            for (int i = 0; i < p; i++)
+                result *= 2;
+            return result;
+        }
+
+
+    }
+
+    class Trial
+    {
+        // A class with absolutely nothing, but only get and sets!
+        public int this[int index]
+        {
+            set { }    // This is kind of weird, you have to admitted.
+        }
+
+        public void action1()
+        {
+            Console.WriteLine("Action 1 is done");
+        }
+
+        public void action2()
+        {
+            Console.WriteLine("Action 2 is done");
+        }
+
+        public void action3()
+        {
+            Console.WriteLine("Action 3 is done");
+        }
+    }
+
+
+    class PropertyClass
+    {
+        /* Most important point: a property is like a method. Hence, it does not define a storage space in the memory. Hence, I need to define a (private)
+           field in the class. Otherwise, the property would be doing some action and allocates no space in the memory! The only exception is of course is the
+           auto implemented properties! */
+        int field;  // This is the private field that is going to be managed by properties.
+                    // The property Myprop makes sure that it would always be positive.
+
+        public int myProp
+        {
+            get { return (myProp); }
+            set
+            {
+                if (value > 0)
+                    field = value;
+                else
+                    field = 0;
+            }
+            
+        }
     }
 }
--- IndexersAndProperties/IndexersAndProperties/Program.cs
+++ IndexersAndProperties/IndexersAndProperties/Program.cs
@@ -6,6 +6,41 @@ namespace IndexersAndProperties
     {
         static void Main(string[] args)
         {
+            // First example
+            Indexers indexers1 = new Indexers(2);
+            Console.WriteLine(indexers1[1]);
+
+            Indexers indexers2 = new Indexers(6);
+            Console.WriteLine(indexers2[5]);
+
+            // Now that we have indexers, we can define an array of a particular object.
+            Indexers[] indexed = new Indexers[2];   // So long as an indexer has not been defined, you cannot create this array!
+            indexed[0] = indexers1;
+            indexed[1] = indexers2;
+            indexed[1].printStatement();    // We can easily access the content of that element.
+
+            foreach (Indexers index in indexed)
+            {
+                Console.WriteLine(index[1]);
+            }
+
+            // A second example, an indexer with no actuall array. every index returns the power of two of that index.
+            IndexerWithNoArray[] indexerWithNo = new IndexerWithNoArray[15];
+            for (int i = 0; i < indexerWithNo.Length; i++)
+            {
+                indexerWithNo[i] = new IndexerWithNoArray();
+                IndexerWithNoArray indexerWithNo1 = indexerWithNo[i];
+                Console.WriteLine(indexerWithNo1[i]);
+            }
+
+            // Third, creating a session from a number of trials.
+            Trial[] session = new Trial[5];
+            for (int i = 0; i < session.Length; i++)
+            {
+                session[i] = new Trial();
+                session[i].action1();
+            }
+            
 
 
             Console.ReadKey();
