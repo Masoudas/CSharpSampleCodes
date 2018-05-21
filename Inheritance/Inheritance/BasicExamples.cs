3a6e49bffcb776294883e5c2decac7632e140af6
masoud aghamohamadian <masoudas@hotmail.com>
2018-05-21 16:40:27 +0430

    Committment is good!

---
+++ Inheritance/Inheritance/BasicExamples.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class TwoDShape
+    {
+        // For now, we neglect constructors.
+        public double Width;
+        public double Height;
+        public void ShowDim()
+        {
+            Console.WriteLine("Width and height are " + Width + " and " + Height);
+        }
+
+    }
+
+    class Triange: TwoDShape
+    {
+        public string Style; // style of triangle
+                             // Return area of triangle.
+        public double Area()
+        {
+            return Width * Height / 2;
+        }
+        // Display a triangle's style.
+        public void ShowStyle()
+        {
+            Console.WriteLine("Triangle is " + Style);
+        }
+    }
+}
--- Inheritance/Inheritance/Program.cs
+++ Inheritance/Inheritance/Program.cs
@@ -7,6 +7,15 @@ namespace Inheritance
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Triange triange = new Triange();
+            triange.Height = 10;
+            triange.Width = 5;
+            triange.Style = "Perpendicular";
+            triange.ShowDim();
+            triange.ShowStyle();
+            Console.WriteLine("Triangle area is " + triange.Area());
+
+            Console.ReadLine();
         }
     }
 }
